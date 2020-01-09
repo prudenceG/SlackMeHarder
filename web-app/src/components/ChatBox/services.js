29684855fba645437f1d26f7e99605f357cdfd1b
Prudence Gaboriau <pgaboriau@imac-de-prudence.home>
2020-01-09 18:10:59 +0100

    add change to test the CD

--- web-app/src/components/ChatBox/services.js
+++ web-app/src/components/ChatBox/services.js
@@ -9,6 +9,7 @@ import {
 } from '../../data/services/api';
 
 export const useMessages = (id, ref) => {
+  console.log('TEST TRAVIS CLASSIC PUSH ON MASTER')
   let channelId = id;
   const [messages, setMessages] = useState([]);
   const [contentValue, setContentValue] = useState('');
