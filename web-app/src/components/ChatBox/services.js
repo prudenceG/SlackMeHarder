fcaa515e2b19d54baccf194d03845386ddce53cd
Prudence Gaboriau <pgaboriau@imac-de-prudence.home>
2020-01-11 13:50:39 +0100

    little change

--- web-app/src/components/ChatBox/services.js
+++ web-app/src/components/ChatBox/services.js
@@ -9,7 +9,7 @@ import {
 } from '../../data/services/api';
 
 export const useMessages = (id, ref) => {
-  console.log('TEST')
+  console.log('TEST TEST')
   let channelId = id;
   const [messages, setMessages] = useState([]);
   const [contentValue, setContentValue] = useState('');
