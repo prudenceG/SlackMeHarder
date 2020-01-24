dd72173e7bbab5f97a9350433d76456e772bb597
PrudenceG <prudence.gaboriau@gmail.com>
2020-01-24 15:30:43 +0100

    indentation controller message

--- api/controllers/messages.js
+++ api/controllers/messages.js
@@ -3,10 +3,10 @@ const messagesService = require('./../services/messages');
 const getMessagesByChannel = async (req, res) => {
   const channelId = req.query.channel_id;
   try {
-      const messages = await messagesService.getMessageByChannel(channelId);
-      res.status(200).send(messages);
-  } catch(error) {
-      res.status(400).json({message: error.message});
+    const messages = await messagesService.getMessageByChannel(channelId);
+    res.status(200).send(messages);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 }
 
@@ -14,11 +14,12 @@ const storeMessage = async (req, res) => {
   const { content, channelId } = req.body;
   const sessionId = req.cookies.sessionId;
   const socket = req.socket;
+
   try {
-      await messagesService.storeMessage(content, channelId, sessionId, socket);
-      res.status(201).send({message: 'Message has been successfuly created'});
-  } catch(error) {
-      res.status(400).json({message: error.message});
+    await messagesService.storeMessage(content, channelId, sessionId, socket);
+    res.status(201).send({ message: 'Message has been successfuly created' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 };
 
@@ -28,11 +29,11 @@ const updateMessage = async (req, res) => {
   const sessionId = req.cookies.sessionId;
   const socket = req.socket;
   try {
-      await messagesService.updateMessage(id, message, sessionId, socket);
-      res.status(201).send('a message has been updated');
-  } catch(error) {
-      res.status(400).json({message: error.message});
-  }    
+    await messagesService.updateMessage(id, message, sessionId, socket);
+    res.status(201).send('a message has been updated');
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 const deleteMessage = async (req, res) => {
@@ -40,10 +41,10 @@ const deleteMessage = async (req, res) => {
   const sessionId = req.cookies.sessionId;
   const socket = req.socket;
   try {
-      await messagesService.deleteMessage(id, sessionId, socket);
-      res.status(200).json({message: 'Message has been successfuly deleted'});
-  } catch(error) {
-      res.status(400).json({message: error.message});
+    await messagesService.deleteMessage(id, sessionId, socket);
+    res.status(200).json({ message: 'Message has been successfuly deleted' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 }
 
